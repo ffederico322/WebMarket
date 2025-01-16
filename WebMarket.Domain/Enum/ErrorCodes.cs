6f7316633de0802dfff2ff8f049a2a062eca531f
Salavat <salavatgumerov14@gmail.com>
2025-01-17 00:58:48 +0300

    создал Resources и добавил ErrorMessages добавил ErrorCodes.cs в Enum

---
+++ WebMarket.Domain/Enum/ErrorCodes.cs
@@ -0,0 +1,10 @@
+﻿namespace WebMarket.Domain.Enum;
+
+public enum ErrorCodes
+{
+    // Products 0 - 9
+    ProductsNotFound = 0,
+    InvalidProductDataError = 1,
+    
+    InternalServerError = 10,
+}
