c130abb46e5596f80ed9b7c98231e905f330b547
Salavat <salavatgumerov14@gmail.com>
2025-01-17 01:02:31 +0300

    создал ProductDto.cs и ProductCreateDto.cs

---
+++ WebMarket.Domain/Dto/Product/ProductCreateDto.cs
@@ -0,0 +1,4 @@
+﻿namespace WebMarket.Domain.Dto.Product;
+
+public record ProductCreateDto(string Name, long CategoryId, string Description, 
+    string Image, decimal Price, int Stock);
---
+++ WebMarket.Domain/Dto/Product/ProductDto.cs
@@ -0,0 +1,4 @@
+﻿namespace WebMarket.Domain.Dto.Product;
+
+public record ProductDto(long Id, string Name, long CategoryId, string Description, string Image, 
+    decimal Price);
