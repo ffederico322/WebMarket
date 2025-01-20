04da41b939fbce21573523a9636ac8c8f8ab23c6
Salavat <salavatgumerov14@gmail.com>
2025-01-20 17:10:35 +0300

    По мелочи

---
+++ WebMarket.Domain/Interfaces/Services/ICartService.cs
@@ -0,0 +1,30 @@
+﻿using WebMarket.Domain.Dto.Cart;
+using WebMarket.Domain.Dto.CartItem;
+using WebMarket.Domain.Result;
+
+namespace WebMarket.Domain.Interfaces.Services;
+
+/// <summary>
+/// Сервис отвечает работу доменной части корзины (Cart)
+/// </summary>
+public interface ICartService
+{
+    /// <summary>
+    /// Получение корзины по ID пользователя 
+    /// </summary>
+    /// <param name="userId"></param>
+    /// <returns></returns>
+    Task<BaseResult<CartDto>> GetCartByUserIdAsync(string userId);
+    
+    Task<BaseResult<CartDto>> AddToCartAsync(string userId, CartItemDto cartItemDto);
+    
+    Task<BaseResult<CartDto>> DeleteCartAsync(string userId, string cartId);
+    
+    Task<BaseResult<CartItemDto>> GetCartItemByIdAsync(string userId, string cartId);
+    
+    Task<CollectionResult<CartItemDto>> GetCartItemsByIdAsync(string userId, string cartId);
+    
+    Task<BaseResult<CartItemDto>> UpdateCartItemAsync(string userId, CartItemDto cartItemDto);
+    
+}
+
---
+++ WebMarket.Domain/Interfaces/Validations/Category/ICategoryValidator.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+using WebMarket.Domain.Entity;
+using WebMarket.Domain.Result;
+
+namespace WebMarket.Domain.Interfaces.Validations;
+
+public interface ICategoryValidator : IBaseValidator<Category>
+{
+    public BaseResult Validate(Category category);
+}
