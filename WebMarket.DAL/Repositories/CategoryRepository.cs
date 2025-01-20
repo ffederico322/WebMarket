68c32aab3d2e7ce4c556c39e6bb85d1e51a7476a
Salavat <salavatgumerov14@gmail.com>
2025-01-20 17:06:38 +0300

    Реализовал метод Exists

---
+++ WebMarket.DAL/Repositories/CategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebMarket.Domain.Entity;
+using WebMarket.Domain.Interfaces.Repositories;
+
+namespace WebMarket.DAL.Repositories;
+
+public class CategoryRepository(ApplicationDbContext dbcontext) : BaseRepository<Category>(dbcontext), ICategoryRepository
+{
+    private readonly ApplicationDbContext _dbContext = dbcontext;
+
+    public async Task<bool> ExistsAsync(int categoryId)
+    {
+        return await _dbContext.Set<Category>().AnyAsync(c => c.Id == categoryId);
+    }
+}
