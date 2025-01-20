a83335be9096634518a4f9d4b7e9c3e52ae60932
Salavat <salavatgumerov14@gmail.com>
2025-01-20 17:03:12 +0300

    Добавил новые названия ошибок для Product

--- WebMarket.Domain/Enum/ErrorCodes.cs
+++ WebMarket.Domain/Enum/ErrorCodes.cs
@@ -5,6 +5,22 @@ public enum ErrorCodes
     // Products 0 - 9
     ProductsNotFound = 0,
     InvalidProductDataError = 1,
+    InvalidProductName = 2,
+    InvalidProductPrice = 3,
+    InvalidProductStock = 4,
+    InvalidProductDescription = 5,
+    ProductNotFound = 6,
     
-    InternalServerError = 10,
+    
+    // Category 
+    CategoryNotFound = 10,
+    
+    // Entity 50 - 60
+    EntityNotFound = 50,
+    
+    // Остальные 80 - 90
+    NoDataProvided = 80,
+    
+    
+    InternalServerError = 100,
 }
