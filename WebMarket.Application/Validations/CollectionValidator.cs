0a1d24848025628d06de26b6fbd80bbe2017e3f7
Salavat <salavatgumerov14@gmail.com>
2025-01-20 17:06:59 +0300

    Добавил валидацию коллекции

---
+++ WebMarket.Application/Validations/CollectionValidator.cs
@@ -0,0 +1,31 @@
+﻿using WebMarket.Application.Resources;
+using WebMarket.Domain.Enum;
+using WebMarket.Domain.Interfaces.Validations;
+using WebMarket.Domain.Result;
+
+namespace WebMarket.Application.Validations;
+
+public class CollectionValidator(IBaseValidator<object> baseValidator)
+{
+    public BaseResult ValidateCollection(params object[] models)
+    {
+        if (models.Length == 0)
+        {
+            return new BaseResult()
+            {
+                ErrorMessage = ErrorMessage.NoDataProvided,
+                ErrorCode = (int)ErrorCodes.NoDataProvided
+            };
+        }
+
+        foreach (var model in models)
+        {
+            var validationResult = baseValidator.ValidateOnNull(model);
+            if (!validationResult.IsSuccess)
+            {
+                return validationResult;
+            }
+        }
+        return new BaseResult();
+    }
+}
